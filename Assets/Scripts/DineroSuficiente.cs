f3d8f4ee138541801d8c89b7b884e64629e9828f
nicokrym <167230566+nicokrym@users.noreply.github.com>
2025-05-05 11:49:37 -0300

    Dinero Suficiente

---
+++ Assets/Scripts/DineroSuficiente.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DineroSuficiente : MonoBehaviour
+{
+    public float dinero;
+    public float precioA;
+    public float precioB;
+    float precioTotal;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        precioTotal = precioA + precioB;
+
+        if (dinero < precioTotal)
+        {
+            Debug.Log("Dinero insuficiente para pagar");
+        }
+        else if (dinero == precioTotal)
+        {
+            Debug.Log("Dinero exacto para pagar");
+        }
+        else if (dinero > precioTotal)
+        {
+            Debug.Log("Dinero de sobra para pagar");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
