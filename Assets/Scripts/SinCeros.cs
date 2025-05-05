c941b54b1376f706b588fe2db4a1e8863df69305
nicokrym <167230566+nicokrym@users.noreply.github.com>
2025-05-05 11:43:00 -0300

    Sin ceros

---
+++ Assets/Scripts/SinCeros.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SinCeros : MonoBehaviour
+{
+    public float numeroA;
+    public float numeroB;
+    bool igualACero;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (numeroB == 0)
+        {
+            igualACero = true;
+            return;
+        }
+
+        if (igualACero != true)
+        {
+            Debug.Log(numeroA / numeroB);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
