276463cff3aa15a8be2f13a649af9842605ee036
nicokrym <167230566+nicokrym@users.noreply.github.com>
2025-05-05 11:38:25 -0300

    Palabra Secreta

---
+++ Assets/Scripts/PalabraSecreta.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PalabraSecreta : MonoBehaviour
+{
+    public string Palabra;
+    string correcta = "domingo";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (Palabra == correcta)
+        {
+            Debug.Log("Has adivinado la palabra");
+        }
+        else
+        {
+            Debug.Log("Palabra incorrecta");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
