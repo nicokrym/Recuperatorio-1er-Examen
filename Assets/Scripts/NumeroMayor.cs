e6cc23ca12d2d9f43f0f11c539624c765513240f
nicokrym <167230566+nicokrym@users.noreply.github.com>
2025-05-05 11:34:08 -0300

    Numero Mayor

---
+++ Assets/Scripts/NumeroMayor.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumeroMayor : MonoBehaviour
+{
+    public int numeroA;
+    public int numeroB;
+    bool aMayor;
+    bool iguales;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (numeroA > numeroB)
+        {
+            aMayor = true;
+        }
+        else if (numeroA < numeroB)
+        {
+            aMayor = false;
+        }
+        else if (numeroA == numeroB)
+        {
+            iguales = true;
+        }
+
+        if (aMayor == true)
+        {
+            Debug.Log("El mayor de los dos números ingresados es " + numeroA);
+        }
+        else if (aMayor == false)
+        {
+            Debug.Log("El mayor de los dos números ingresados es " + numeroB);
+        }
+        else if (iguales == true)
+        {
+            Debug.Log("Los número son iguales entre sí");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
